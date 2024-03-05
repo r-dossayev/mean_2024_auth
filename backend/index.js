1401615279aaa3107d6c85b353f6a4fa2155a3b5
mallemes <dosaev.rysbek5@gmail.com>
2024-03-05 22:49:50 +0600

    using dotenv

--- backend/index.js
+++ backend/index.js
@@ -2,14 +2,20 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-mongoose.connect('mongodb+srv://rysbekdossayev:Laravel5@cluster0.guazwsy.mongodb.net/?retryWrites=true&w=majority',)
+const http = require('http');
+require('dotenv').config();
+const socketIo = require('socket.io');
+
+
+mongoose.connect(process.env.MONGO_URI,)
   .then(() => console.log("Соединение с базой данных установлено"))
   .catch(err => console.error("Ошибка подключения к базе данных:", err));
 
 const routes = require('./routes/routes');
 
 const app = express();
-const port = 8787;
+// const server = http.createServer(app);
+const port = process.env.PORT ||8686;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -24,3 +30,6 @@ app.use('/api', routes);
 app.listen(port, () => {
   console.log(`app listening at http://localhost:${port}`);
 });
+// server.listen(3001, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
